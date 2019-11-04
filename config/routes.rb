cb6a8dc51fb905255b5912e3fb56c65821e60fe7
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-11-04 17:32:00 +0100

    coding controller Services, routes, and Services views for index and new

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   devise_for :users
-  get 'service_requests/new'
-  get 'service_requests/create'
-  get 'service_requests/show'
-  get 'service_requests/destroy'
-  get 'services/index'
-  get 'services/new'
-  get 'services/create'
-  get 'services/show'
-  get 'services/update'
-  get 'services/destroy'
-  get 'services/edit'
-  root to: 'pages#home'
+
+  root to: 'servicess#index'
+  resources :services, only: [:index, :show, :new, :create] do
+    resources :service_requests, only: [:create, :destroy, :new, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
