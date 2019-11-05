3cb01bf9b8f3c614b7347f1a93a30826f7fe0ffa
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-11-05 14:42:23 +0100

    adding category drop down list

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,7 @@
 class Service < ApplicationRecord
+  CATEGORIES = ["Renewal", "Technical Review", "Healthcare", "Claim Experience", "Program Set-up"]
   belongs_to :user
   has_many :service_requests
-  validates :name, :description, :rate_per_day, :category, :address, presence: true
+  validates :name, :description, :rate_per_day, :address, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
-
