ecd18fe3fea0ea239582960945cbaf1efb006601
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-11-07 18:08:55 +0100

    delete and remove

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,9 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create
+  def edit_offered_service
+    @user = current_user
+    authorize @user
   end
 
   def remove_offered_service
@@ -12,7 +14,7 @@ class UsersController < ApplicationController
     authorize @user
     offered_service = find_service
     offered_service.destroy
-    redirect_to '/user/services', :notice => "Service successfully removed from your list"
+    redirect_to '/user/services'
   end
 
   private
